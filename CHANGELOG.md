4548479a8241351a4e67c71fa5dbe83f917fec62
Ahmad-whizpool <ahmad.khan@whizpool.com>
2025-03-28 12:06:38 +0500

    FIx registerChannel issue

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,8 @@
 ## 0.0.1
-
 - Updated Kotlin Version
 
 ## 0.0.2
-
 - Add namespace
+
+## 0.0.3
+- Fix registerChannel issue
--- android/src/main/kotlin/com/whizpool/disk_space_upgrade/DiskSpacePlugin.kt
+++ android/src/main/kotlin/com/whizpool/disk_space_upgrade/DiskSpacePlugin.kt
@@ -3,30 +3,23 @@ package com.whizpool.disk_space_upgrade
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.PluginRegistry
 
-class DiskSpacePlugin: FlutterPlugin {
+class DiskSpacePlugin : FlutterPlugin {
 
-  companion object {
     private var channel: MethodChannel? = null
-    private var handler: MethodHandlerImpl = MethodHandlerImpl()
+    private val handler = MethodHandlerImpl()
 
-    @JvmStatic
-    fun registerWith(registrar: PluginRegistry.Registrar) {
-      registerChannel(registrar.messenger())
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        registerChannel(binding.binaryMessenger)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel?.setMethodCallHandler(null)
+        channel = null
     }
 
     private fun registerChannel(messenger: BinaryMessenger) {
-      channel = MethodChannel(messenger, "disk_space_upgrade")
-      channel!!.setMethodCallHandler(handler)
+        channel = MethodChannel(messenger, "disk_space_upgrade")
+        channel?.setMethodCallHandler(handler)
     }
-  }
-
-  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    registerChannel(binding.binaryMessenger)
-  }
-
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel = null
-  }
 }
